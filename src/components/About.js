5152a17cc4848b56aa79ea3640d8ba6102ad21d3
MrVictor42 <guin-hu@hotmail.com>
2021-02-01 12:26:13 -0300

    Remove cvs frontend and backend

--- src/components/About.js
+++ src/components/About.js
@@ -1,71 +1,41 @@
 import React, { Component } from 'react';
 import { FormattedMessage } from 'react-intl';
 
-import CVBackendPtBr from '../files/backend/CV_Victor_Mota_pt_br.pdf';
-import CVBackendDeDe from '../files/backend/CV_Victor_Mota_de_de.pdf';
-import CVBackendEnUs from '../files/backend/CV_Victor_Mota_en_us.pdf';
-import CVBackendEsEs from '../files/backend/CV_Victor_Mota_es_es.pdf';
-import CVBackendFrFr from '../files/backend/CV_Victor_Mota_fr_fr.pdf';
-import CVBackendItIt from '../files/backend/CV_Victor_Mota_it_it.pdf';
-
-import CVFrontendPtBr from '../files/frontend/CV_Victor_Mota_pt_br.pdf';
-import CVFrontendDeDe from '../files/frontend/CV_Victor_Mota_de_de.pdf';
-import CVFrontendEnUs from '../files/frontend/CV_Victor_Mota_en_us.pdf';
-import CVFrontendEsEs from '../files/frontend/CV_Victor_Mota_es_es.pdf';
-import CVFrontendFrFr from '../files/frontend/CV_Victor_Mota_fr_fr.pdf';
-import CVFrontendItIt from '../files/frontend/CV_Victor_Mota_it_it.pdf';
-
-import CVFullPtPt from '../files/fullStack/CV_Victor_Mota_pt_br.pdf';
-import CVFullDeDe from '../files/fullStack/CV_Victor_Mota_de_de.pdf';
-import CVFullEnUs from '../files/fullStack/CV_Victor_Mota_en_us.pdf';
-import CVFullEsEs from '../files/fullStack/CV_Victor_Mota_es_es.pdf';
-import CVFullFrFr from '../files/fullStack/CV_Victor_Mota_fr_fr.pdf';
-import CVFullItIt from '../files/fullStack/CV_Victor_Mota_it_it.pdf';
+import CVPtbr from '../files/CV_Victor_Mota_pt_br.pdf';
+import CVDeDe from '../files/CV_Victor_Mota_de_de.pdf';
+import CVEnUs from '../files/CV_Victor_Mota_en_us.pdf';
+import CVEsEs from '../files/CV_Victor_Mota_es_es.pdf';
+import CVFrFr from '../files/CV_Victor_Mota_fr_fr.pdf';
+import CVItIt from '../files/CV_Victor_Mota_it_it.pdf';
 
 class About extends Component {
 
 	render() {
 
 		const Profile_pic = 'images/profile_pic.jpg';
-		let resumecvbackend = '';
-		let resumecvfrontend = '';
-		let resumecvfull = '';
+		let resumecv = '';
 
 		switch(this.props.country) {
 			case 'pt-br':
-				resumecvbackend = CVBackendPtBr;
-				resumecvfrontend = CVFrontendPtBr;
-				resumecvfull = CVFullPtPt;
+				resumecv = CVPtbr;
 				break;
 			case 'en-us':
-				resumecvbackend = CVBackendEnUs;
-				resumecvfrontend = CVFrontendEnUs;
-				resumecvfull = CVFullEnUs;
+				resumecv = CVEnUs;
 				break;
 			case 'de-de':
-				resumecvbackend = CVBackendDeDe;
-				resumecvfrontend = CVFrontendDeDe;
-				resumecvfull = CVFullDeDe;
+				resumecv = CVDeDe;
 				break;
 			case 'es-es':
-				resumecvbackend = CVBackendEsEs;
-				resumecvfrontend = CVFrontendEsEs;
-				resumecvfull = CVFullEsEs;
+				resumecv = CVEsEs;
 				break;
 			case 'fr-fr':
-				resumecvbackend = CVBackendFrFr;
-				resumecvfrontend = CVFrontendFrFr;
-				resumecvfull = CVFullFrFr;
+				resumecv = CVFrFr;
 				break;
 			case 'it-it':
-				resumecvbackend = CVBackendItIt;
-				resumecvfrontend = CVFrontendItIt;
-				resumecvfull = CVFullItIt;
+				resumecv = CVItIt;
 				break;
 			default:
-				resumecvbackend = CVBackendPtBr;
-				resumecvfrontend = CVFrontendPtBr;
-				resumecvfull = CVFullPtPt;
+				resumecv = CVPtbr;
 		}
 		
 		return (
@@ -97,18 +67,8 @@ class About extends Component {
 							</div>
 							<div className = 'columns download'>
 								<p>
-									<a href = { resumecvfrontend } className = 'button' 					target = '_blank'>
-										<i className = 'fa fa-download'></i>Download CV Frontend
-									</a>
-								</p>
-								<p>
-									<a href = { resumecvbackend } className = 'button' 					target = '_blank'>
-										<i className = 'fa fa-download'></i>Download CV Backend
-									</a>
-								</p>
-								<p>
-									<a href = { resumecvfull } className = 'button' 					target = '_blank'>
-										<i className = 'fa fa-download'></i>Download CV FullStack
+									<a href = { resumecv } className = 'button' 					target = '_blank'>
+										<i className = 'fa fa-download'></i>Download CV 
 									</a>
 								</p>
 							</div>
