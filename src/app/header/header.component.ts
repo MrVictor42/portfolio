04fd5894609dbe7dc9bc3ad09d1e4aa16e4b5675
MrVictor42 <guin-hu@hotmail.com>
2023-02-28 23:15:02 -0300

    Changing languages on header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import portuguese from '../../assets/i18n/pt-BR.json';
+import english from '../../assets/i18n/en-US.json';
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+	selector: 'app-header',
+	templateUrl: './header.component.html',
+	styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
 
+	@Input() language : string = "pt-BR";
+	@Input() locale : any;
+
+	ngOnInit(): void {
+		this.locale = portuguese;
+	}
+
+	selectedLanguage(selectedLanguage : string) {
+		switch(selectedLanguage) {
+			case 'pt-BR' : {
+				this.locale = portuguese;
+				break;
+			}
+			case 'en-US' : {
+				this.locale = english;
+				break;
+			}
+			default: {
+				this.locale = portuguese;
+				break;
+			}
+		}
+	}
 }
