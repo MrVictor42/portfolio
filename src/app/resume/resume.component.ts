6f548579c41dc5e38a2431d3da4ec6affc775770
MrVictor42 <guin-hu@hotmail.com>
2023-03-03 22:10:21 -0300

    Finishing experiences and starting skills with images

--- src/app/resume/resume.component.ts
+++ src/app/resume/resume.component.ts
@@ -10,6 +10,8 @@ export class ResumeComponent {
 
 	locale: any;
 	educations: Dictionary | any;
+	experience : Dictionary | any;
+	skills : Dictionary | any;
 
 	constructor(private languageService: LanguageService) {
 
@@ -19,11 +21,21 @@ export class ResumeComponent {
 		this.languageService.currentLanguage.subscribe(language => {
 			this.locale = language;
 			this.educations = this.locale.resumePage.education;
+			this.experience = this.locale.resumePage.experience;
+			this.skills = this.locale.resumePage.skills;
 		});
 	}
 
-	keys(): Array<string> {
+	keysEducation(): Array<string> {
 		return Object.keys(this.educations!!);
+	}
+
+	keysWorks(): Array<string> {
+		return Object.keys(this.experience!!);
+	}
+
+	keysSkills(): Array<string> {
+		return Object.keys(this.skills!!);
 	}
 }
 
